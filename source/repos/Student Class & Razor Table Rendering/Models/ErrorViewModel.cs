b074c79fd94a296c9e06c93c5db466b04ce80e5a
TechTinker07 <cherryqui25@gmail.com>
2025-08-30 10:37:32 +0800

    Delete source directory

--- source/repos/Student Class & Razor Table Rendering/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Student_Class___Razor_Table_Rendering.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
